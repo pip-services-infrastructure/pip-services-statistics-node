09d94b8af04abea247cd43535c92c6a9fb1a45e6
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-09 15:51:28 +0300

    added elasticsearch to config

--- src/container/StatisticsProcess.ts
+++ src/container/StatisticsProcess.ts
@@ -1,5 +1,6 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { StatisticsServiceFactory } from '../build/StatisticsServiceFactory';
 
@@ -8,6 +9,7 @@ export class StatisticsProcess extends ProcessContainer {
     public constructor() {
         super("statistics", "Statistics microservice");
         this._factories.add(new StatisticsServiceFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 
 
