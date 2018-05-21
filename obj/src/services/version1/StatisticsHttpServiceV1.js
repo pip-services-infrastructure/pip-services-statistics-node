c5d1c926583da03274d64e04efe87a1d9b5584a9
pipdeveloper <pipdevs@gmail.com>
2018-05-21 11:35:55 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/StatisticsHttpServiceV1.js
+++ obj/src/services/version1/StatisticsHttpServiceV1.js
@@ -4,7 +4,7 @@ const pip_services_commons_node_1 = require("pip-services-commons-node");
 const pip_services_net_node_1 = require("pip-services-net-node");
 class StatisticsHttpServiceV1 extends pip_services_net_node_1.CommandableHttpService {
     constructor() {
-        super('statistics');
+        super('v1/statistics');
         this._dependencyResolver.put('controller', new pip_services_commons_node_1.Descriptor('pip-services-statistics', 'controller', 'default', '*', '1.0'));
     }
 }
