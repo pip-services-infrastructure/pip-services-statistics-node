ddd3f6e25fd910064e0c2d2f9f8057e36eb8d0be
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:29:33 +0300

    added DefaultRpcFactory

--- obj/src/container/StatisticsProcess.js
+++ obj/src/container/StatisticsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const StatisticsServiceFactory_1 = require("../build/StatisticsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class StatisticsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("statistics", "Statistics microservice");
         this._factories.add(new StatisticsServiceFactory_1.StatisticsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.StatisticsProcess = StatisticsProcess;
--- src/container/StatisticsProcess.ts
+++ src/container/StatisticsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { StatisticsServiceFactory } from '../build/StatisticsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class StatisticsProcess extends ProcessContainer {
 
     public constructor() {
         super("statistics", "Statistics microservice");
         this._factories.add(new StatisticsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 
